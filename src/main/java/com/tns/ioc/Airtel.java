dc6481353c817789361e07a68a8e623c22cfb845
SabaTabassum98 <khalidzeeba@gmail.com>
2022-04-21 16:37:20 +0530

    done ioc

---
+++ src/main/java/com/tns/ioc/Airtel.java
@@ -0,0 +1,15 @@
+package com.tns.ioc;
+
+public class Airtel implements Sim {
+
+	public void calling() {
+		System.out.println("calling with Airtel sim ");
+		
+	}
+
+	public void browsing() {
+		
+		System.out.println("browsing with Airtel sim ");
+	}
+
+}
